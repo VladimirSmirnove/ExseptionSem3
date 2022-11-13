71da28c9383f9167717d992711d0a9285c68b420
Vladimir <vladsmir323@gmail.com>
2022-11-13 09:42:40 +0300

    Config

---
+++ Config.java
@@ -0,0 +1,13 @@
+import java.text.SimpleDateFormat;
+
+public class Config {
+    private static int arrLength;
+
+    public static void setArrLength(int arrLength) {
+        Config.arrLength = arrLength;
+    }
+
+    public static int getArrLength() {
+        return arrLength;
+    }
+}
